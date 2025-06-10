bfd88fe679e6d297ad559f1bdd39d14dc76f7093
willamette <94886283+willamettefour@users.noreply.github.com>
2025-06-10 15:13:19 -0700

    refresh

--- messageslog/__init__.py
+++ messageslog/__init__.py
@@ -1,11 +1,12 @@
 from .messageslog import MessagesLog
+import discord
 
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
+__red_end_user_data_statement__ = ("This cog does not persistently store data or metadata about users.")
 
 async def setup(bot):
     cog = MessagesLog(bot)
     await cog.initialize()
-    bot.add_cog(cog)
+    if discord.__version__[0] == "2":
+        await bot.add_cog(cog)
+    else:
+        bot.add_cog(cog)
--- translators/__init__.py
+++ translators/__init__.py
@@ -1,9 +1,10 @@
 from .translators import Translators
+import discord
 
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
+__red_end_user_data_statement__ = ("This cog does not persistently store data or metadata about users.")
 
-
-def setup(bot):
-    bot.add_cog(Translators(bot))
+async def setup(bot):
+    if discord.__version__[0] == "2":
+        await bot.add_cog(Translators(bot))
+    else:
+        bot.add_cog(Translators(bot))
